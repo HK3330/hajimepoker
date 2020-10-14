9cbf90de53113e9f2f8ec6e1b01735f811b56474
小松初 <komatsuhajime@MacBook-Pro.local>
2020-10-15 00:07:47 +0900

    動作自体はよさそう

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -25,17 +25,17 @@ $('.card').on({
     $(".select_num").remove();
     var select_num_div = document.createElement('div');
     select_num_div.className = 'select_num';
-    select_num_div.innerHTML = 'あなたが選んだのは ' + card_num;
+    select_num_div.innerHTML = 'Your Choice ' + card_num;
     info_panel.appendChild(select_num_div);
 }})
 // カード情報受信
 socketio.on('result_card_list',function(result_arr){
-    console.log('クライアント' + result_arr[0] + result_arr[1]);
+    // console.log('クライアント' + result_arr[0] + result_arr[1]);
     // 番号と名前をセッションに入れる
     window.sessionStorage.setItem(['result_number'],[result_arr[0]]);
     window.sessionStorage.setItem(['result_user'],[result_arr[1]]);
     // カードを表示する
-    cardLineUp()
+    selectCardLineUp()
 });
 
 // オープンボタンを押したとき
@@ -46,6 +46,8 @@ document.getElementById("open").onclick = function() {
 }
 //　オープン情報受信
 socketio.on('open',function(){
+    // ここで名前をキーに数字を入れる
+    openCardLineUp()
     $('.back').removeClass('back');
 });
 
@@ -66,7 +68,40 @@ socketio.on('reset',function(){
 });
 
 // セッションの情報でカードを並べる
-function cardLineUp() {
+function selectCardLineUp() {
+    // var input_name = document.getElementById("input_name").value;
+    // 配下をすべて削除
+    $('.result_bord').empty();
+    // セッションから値(str)を取る
+    var result_user_str = window.sessionStorage.getItem(['result_user']);
+
+    // strを配列化
+    result_user = result_user_str.split(',');
+    if (result_user[0] != ''){
+    
+        for (let i = 0; i < result_user.length; i++) {
+            // 大本のdiv
+            var frame_div = document.createElement('div');
+            frame_div.className = 'frame';
+            
+            // 数字のdiv
+            var result_card_div = document.createElement('div');
+            result_card_div.className = 'card back';
+            frame_div.appendChild(result_card_div);
+    
+            // 名前のp
+            var card_name_p = document.createElement('p');
+            card_name_p.className = 'card_name';
+            card_name_p.innerHTML = result_user[i];
+            frame_div.appendChild(card_name_p);
+    
+            // result_bordに追加
+            panel.appendChild(frame_div);
+        }
+    }
+}
+
+function openCardLineUp() {
     // var input_name = document.getElementById("input_name").value;
     // 配下をすべて削除
     $('.result_bord').empty();
@@ -76,6 +111,9 @@ function cardLineUp() {
 
     // strを配列化
     result_number = result_number_str.split(',');
+    // [TODO] ソートするならresult_numberの中身をstrをintに変換
+    //// ∞は最後、?は最後から2番目とか？
+    ////// そうするとnameも配列を入れ替えないといけないな。 
     result_user = result_user_str.split(',');
     if (result_number[0] != ''){
     
@@ -86,7 +124,7 @@ function cardLineUp() {
             
             // 数字のdiv
             var result_card_div = document.createElement('div');
-            result_card_div.className = 'card back';
+            result_card_div.className = 'card back result';
             result_card_div.innerHTML = result_number[i];
             frame_div.appendChild(result_card_div);
     
@@ -110,7 +148,7 @@ if (window.performance) {
       card_num = window.sessionStorage.getItem(['select_num']);
       var select_num_div = document.createElement('div');
       select_num_div.className = 'select_num';
-      select_num_div.innerHTML = 'あなたが選んだのは ' + card_num;
+      select_num_div.innerHTML = 'Your Choice ' + card_num;
       info_panel.appendChild(select_num_div);
     } else {
       // リロードされていない
